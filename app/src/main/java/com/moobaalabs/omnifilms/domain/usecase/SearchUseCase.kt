7c3afa8aec4e2fb2453e50f928509ea74a0ca6ff
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2025-01-18 21:13:40 -0300

    project domain / usecase / getFavorite, getRecentlySearched, movieRepository interface, removeFavorite, removeSearched, saveFavorite, saveRecentrySearched, searchUseCase

---
+++ app/src/main/java/com/moobaalabs/omnifilms/domain/usecase/SearchUseCase.kt
@@ -0,0 +1,18 @@
+package com.moobaalabs.omnifilms.domain.usecase
+
+import com.moobaalabs.omnifilms.data.remote.response.MovieResponse
+import com.moobaalabs.omnifilms.domain.repository.MovieRepository
+import com.moobaalabs.omnifilms.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SearchMoviesUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    suspend fun execute(query: String): Resource<MovieResponse> {
+        return repository.searchMoviesByTitle(query)
+    }
+}
+
