05441cdf7b56f77d925d00a30c067ed5ec989237
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2025-01-18 21:08:11 -0300

    project utils and colors

---
+++ app/src/main/java/com/moobaalabs/omnifilms/presentation/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.moobaalabs.omnifilms.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF326afd)
+val PrimaryGrey = Color(0xFFE5E5E5)
+val SecondaryGreen = Color(0xFF92ec47)
+val TerciaryDarkGray = Color(0xFF0c1419)
+val SecondaryPink = Color(0xFFE91E63)
+val TerciaryBlue = Color(0xFF0069DC)
+val SecondaryRed = Color(0xFFF10130)
---
+++ app/src/main/java/com/moobaalabs/omnifilms/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.moobaalabs.omnifilms.util
+
+object Constants {
+    const val BASE_URL = "https://www.omdbapi.com/?"
+}
