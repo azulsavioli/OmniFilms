a7cb79988effc49ab975350cd9e650e7b6bfe493
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2025-01-18 21:17:39 -0300

    remove unused imports

--- app/src/main/java/com/moobaalabs/omnifilms/domain/usecase/SearchUseCase.kt
+++ app/src/main/java/com/moobaalabs/omnifilms/domain/usecase/SearchUseCase.kt
@@ -3,7 +3,6 @@ package com.moobaalabs.omnifilms.domain.usecase
 import com.moobaalabs.omnifilms.data.remote.response.MovieResponse
 import com.moobaalabs.omnifilms.domain.repository.MovieRepository
 import com.moobaalabs.omnifilms.util.Resource
-import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 
